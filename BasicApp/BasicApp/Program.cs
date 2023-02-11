9efb4d077e3ed86c18a773836ccadebb324a7048
user <user@MOJ-LAPTOP>
2023-02-11 11:52:53 +0100

    Homework - day 4

---
+++ BasicApp/BasicApp/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string name = "Mat196";
+int age = 21;
+bool isMan = true;
+
+if (age < 30 && isMan == false)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && isMan == true)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Inna konfiguracja");
+}
