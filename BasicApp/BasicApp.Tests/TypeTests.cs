a642aae61a4153d70245883120d9743ec812d57c
user <user@MOJ-LAPTOP>
2023-02-14 13:59:01 +0100

    Homework - day 8

---
+++ BasicApp/BasicApp.Tests/TypeTests.cs
@@ -0,0 +1,54 @@
+﻿namespace BasicApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void RefferenceTestForInt()
+        {
+            // arrange
+            int number1 = 3; int number2 = 3;
+
+            // act
+
+            // assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void RefferenceTestForFloat()
+        {
+            // arrange
+            float number1 = 3; float number2 = 3;
+
+            // act
+
+            // assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void RefferenceTestForString()
+        {
+            // arrange
+            string str1 = "abc"; string str2 = "abc";
+
+            // act
+
+            // assert
+            Assert.AreEqual(str1, str2);
+        }
+
+        [Test]
+        public void RefferenceTestForEmployee()
+        {
+            // arrange
+            Employee emp1 = new Employee("Jan", "Kowalski", 35);
+            Employee emp2 = new Employee("Jan", "Kowalski", 35);
+
+            // act
+
+            // assert
+            Assert.AreNotEqual(emp1, emp2);
+        }
+    }
+}
